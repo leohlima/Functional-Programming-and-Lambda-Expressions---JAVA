044071bad42066b1cb636501706cab43a2f05bfd
Leonardo Araujo <leolimalp@hotmail.com>
2022-03-21 11:31:34 -0300

    streams

--- 2_LambdaInStream/Stream/src/Program/App.java
+++ 2_LambdaInStream/Stream/src/Program/App.java
@@ -1,7 +1,32 @@
 package Program;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class App {
     public static void main(String[] args) {
-        
+        List<Integer> list = Arrays.asList(3, 4, 8, 9, 15);
+
+        //Stream from Integer
+        Stream<Integer> st1 = list.stream();
+        System.out.println(Arrays.toString(st1.toArray()));
+
+        //Applying map
+        Stream<Integer> st2 = list.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(st2.toArray()));
+
+        //Putting elements directly on stream
+        Stream<String> st3 = Stream.of("Celso", "Carlos", "Leo");
+        System.out.println(Arrays.toString(st3.toArray()));
+
+        //Stream witch iterate and a limit of elements
+        Stream<Integer> st4 = Stream.iterate(0, x -> x + 2);
+        System.out.println(Arrays.toString(st4.limit(10).toArray()));
+
+        //Fibonacci Sequence
+        Stream<Long> st5 = Stream.iterate(new long[]{ 0L, 1L }, p->new long[]{ p[1], p[0]+p[1] }).map(p -> p[0]);
+        System.out.println(Arrays.toString(st5.limit(20).toArray()));
     }
 }
