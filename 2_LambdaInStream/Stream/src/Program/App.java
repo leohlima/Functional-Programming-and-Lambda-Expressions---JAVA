a2fdb31916be4cd11156596697384a473aa31a8e
Leonardo Araujo <leolimalp@hotmail.com>
2022-03-21 13:22:34 -0300

    pipeline

--- 2_LambdaInStream/Stream/src/Program/App.java
+++ 2_LambdaInStream/Stream/src/Program/App.java
@@ -3,6 +3,7 @@ package Program;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
@@ -28,5 +29,12 @@ public class App {
         //Fibonacci Sequence
         Stream<Long> st5 = Stream.iterate(new long[]{ 0L, 1L }, p->new long[]{ p[1], p[0]+p[1] }).map(p -> p[0]);
         System.out.println(Arrays.toString(st5.limit(20).toArray()));
+
+        //Pipeline: when you have more than one intermediate operations and one terminal operation
+        Stream<Integer> st6 = list.stream();
+        int sum = st6.reduce(0, (x, y) -> x + y);
+        System.out.println("Sum = " + sum);
+        List<Integer> newList = list.stream().filter(x -> x % 2 == 0).map(x -> x * 10).toList();
+        System.out.println(Arrays.toString(newList.toArray()));
     }
 }
