b300e6f5a0b58c552398a8d03f8d4a70c3fe4bdc
Leonardo Araujo <leolimalp@hotmail.com>
2022-03-23 15:37:35 -0300

    resolving 2 exercise

--- 2_LambdaInStream/2_Exercise/src/Program/App.java
+++ 2_LambdaInStream/2_Exercise/src/Program/App.java
@@ -1,7 +1,47 @@
 package Program;
 
+import entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class App {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter full file path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Employee> list = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(" ");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+
+            System.out.print("Enter salary: ");
+            double salary = sc.nextDouble();
+
+            List<String> email = list.stream()
+                    .filter(x -> x.getSalary() > salary)
+                    .map(x -> x.getEmail())
+                    .sorted()
+                    .toList();
+
+            System.out.println("Email of people whose salary is more than " + String.format("%.2f:", salary));
+            email.forEach(System.out::println);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        sc.close();
     }
 }
