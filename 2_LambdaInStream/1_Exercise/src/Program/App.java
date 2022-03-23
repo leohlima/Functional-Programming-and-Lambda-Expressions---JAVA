2acfa4b6fb45d93fce6961aba591baf161b6cf9d
Leonardo Araujo <leolimalp@hotmail.com>
2022-03-22 23:45:01 -0300

    formating

--- 2_LambdaInStream/1_Exercise/src/Program/App.java
+++ 2_LambdaInStream/1_Exercise/src/Program/App.java
@@ -3,7 +3,6 @@ package Program;
 import entities.Product;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -17,17 +16,16 @@ public class App {
         String path = sc.nextLine();
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
             List<Product> list = new ArrayList<>();
-
+            
             String line = br.readLine();
-            while (line != null){
-                String[] fields  = line.split(",");
+            while (line != null) {
+                String[] fields = line.split(",");
                 list.add(new Product(fields[0], Double.parseDouble(fields[1])));
                 line = br.readLine();
             }
 
-            double avg = list.stream().map(p -> p.getPrice()).reduce(0.0, (x ,y) -> x + y) / list.size();
+            double avg = list.stream().map(p -> p.getPrice()).reduce(0.0, (x, y) -> x + y) / list.size();
             System.out.println("Average price: " + String.format("%.2f", avg));
 
             Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
@@ -39,7 +37,7 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         sc.close();
     }
 }
--- 2_LambdaInStream/Stream/src/Program/App.java
+++ 2_LambdaInStream/Stream/src/Program/App.java
@@ -1,9 +1,7 @@
 package Program;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
